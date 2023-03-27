bf3e3daa740b38dc52b939f21a257c23472fb8dc
DenisWCS1 <denis.perrisseau.dev@gmail.com>
2023-03-27 12:07:06 +0200

    push quest

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # tsquests2
+# $ node challenge.js
+# Pierre is 23 years old
+# Paul is 40 years old
+# Jacques is 25 years old
---
+++ challenge.ts
@@ -0,0 +1,22 @@
+// challenge.ts
+const prettyPrintWilder = (users) => {
+    users.map((user) => {
+      console.log(`${user.name} is ${user.age} years old`);
+    });
+  };
+  
+  const wilders :any[] = [];
+  interface user {
+    name: string;
+    age:number;
+  }
+  const user1 = { name: "Pierre", age: 23 };
+ //const user2 = { name: "Paul", birthday: "10/02/1990" };
+const user2 = { name: "Paul", age: 40  };
+  const user3 = { name: "Jacques", age: 25 };
+  wilders.push(user1);
+  wilders.push(user2);
+  wilders.push(user3);
+  prettyPrintWilder(wilders);
+
+  
